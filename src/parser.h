3bd460be604248b220d14d9a4fe0622d3c56ea8b
PANFACTORY <yutatanabe510@gmail.com>
2022-09-26 17:43:02 +0900

    字句解析器を別ファイルに分離

--- src/parser.h
+++ src/parser.h
@@ -5,53 +5,9 @@
 #include <sstream>
 #include <cctype>
 
-namespace JsonParser {
-    // LexicalAnalyser
-    std::vector<std::string> LexicalAnalyser(std::stringstream &ss) {
-        std::vector<std::string> chs;
-        char ch = ss.get();
-        while (!ss.eof()) {
-            if (ch == ' ' || ch == '\t' || ch == '\n') {
-                ch = ss.get();
-                continue;
-            } else if (ch == '"') {
-                std::string str("\"");
-                ch = ss.get();
-                while (!ss.eof() && ch != ':' && ch != '}' && ch != ',' && ch != ']') {
-                    str += ch;
-                    ch = ss.get();
-                }
-                // TODO: Trim spaces at end of str.
-                // TODO: Check end of str is \".
-                chs.push_back(str);
-            } else if (std::isdigit(ch) || ch == '-') {
-                std::string str(&ch);
-                ch = ss.get();
-                while (!ss.eof() && ch != '}' && ch != ',' && ch != ']') {
-                    str += ch;
-                    ch = ss.get();
-                }
-                // TODO: Trim spaces at end of str.
-                // TODO: str is number.
-                chs.push_back(str);
-            } else if (ch == 't' || ch == 'f' || ch == 'n') {
-                std::string str(&ch);
-                ch = ss.get();
-                while (!ss.eof() && ch != '}' && ch != ',' && ch != ']') {
-                    str += ch;
-                    ch = ss.get();
-                }
-                // TODO: Trim spaces at end of str.
-                // TODO: str is "true", "false" or "null".
-                chs.push_back(str);
-            } else {
-                chs.push_back(std::string(&ch));
-                ch = ss.get();
-            }
-        }
-        return chs;
-    }
+#include "lexical.h"
 
+namespace JsonParser {
     // Parser
     std::vector<std::string> Parser(std::stringstream &ss) {
         // Lexical analyse
