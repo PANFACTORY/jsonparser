ddbe1ab2b716a7d7f2f59e9be3218437ba3988a7
PANFACTORY <yutatanabe510@gmail.com>
2022-09-27 07:59:29 +0900

    parserのテストを修正

--- src/parser.h
+++ src/parser.h
@@ -11,7 +11,7 @@ namespace JsonParser {
     int Value(int index, const std::vector<std::string> &chs, Node *node);
 
     int Object(int index, const std::vector<std::string> &chs, Node *parent) {
-        std::cout << "Object is called " << index << std::endl;
+        std::cout << "Object is called and index is " << index << std::endl;
         switch (chs[index][0]) {
         case '}':
             return index + 1;
@@ -31,21 +31,35 @@ namespace JsonParser {
             return index_current + 1;
             }
         default:
-            std::cout << "default at Object " << index << std::endl;
+            std::cout << "default at Object and index is " << index << std::endl;
             return 0;
         }
     }
 
     int Value(int index, const std::vector<std::string> &chs, Node *node) {
-        std::cout << "Value is called " << index << std::endl;
+        std::cout << "Value is called and index is " << index << std::endl;
         switch (chs[index][0]) {
         case '"':
+        case '-':
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case 'n':
+        case 't':
+        case 'f':
             node->SetValue(chs[index]);
             return index + 1;
         case '{':
             return Object(index + 1, chs, node);
         default:
-            std::cout << "default at Value " << index << std::endl;
+            std::cout << "default at Value and index is " << index << std::endl;
             return 0;
         }
     }
--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -3,43 +3,129 @@
 #include "../src/parser.h"
 
 TEST(JsonParserTest, ParserTest1) {
-    std::vector<std::string> chs = {"\"ABC\""};
-    JsonParser::Node* root = JsonParser::Parser(chs);
-    std::cout << *root << std::endl;
-    ASSERT_TRUE(true);
+    std::vector<std::string> chs = {
+        "{",
+        "\"name\"",
+        ":",
+        "\"Tanaka\"",
+        "}"
+    };
+    JsonParser::Node root("");
+    ASSERT_EQ(JsonParser::Value(0, chs, &root), chs.size());
+    std::cout << root << std::endl;
 }
 
 TEST(JsonParserTest, ParserTest2) {
-    std::vector<std::string> chs = {"{", "}"};
-    JsonParser::Node* root = JsonParser::Parser(chs);
-    std::cout << *root << std::endl;
-    ASSERT_TRUE(true);
+    std::vector<std::string> chs = {
+        "{",
+        "\"name\"",
+        ":",
+        "\"Tanaka\"",
+        ",",
+        "\"age\"",
+        ":",
+        "26",
+        "}"
+    };
+    JsonParser::Node root("");
+    ASSERT_EQ(JsonParser::Value(0, chs, &root), chs.size());
+    std::cout << root << std::endl;
 }
 
 TEST(JsonParserTest, ParserTest3) {
-    std::vector<std::string> chs = {"{", "\"name\"", ":", "\"Tanaka\"", "}"};
-    JsonParser::Node* root = JsonParser::Parser(chs);
-    std::cout << *root << std::endl;
-    ASSERT_TRUE(true);
+    std::vector<std::string> chs = {
+        "[",
+        "\"ABC\"",
+        ",",
+        "\"DEF\"",
+        "]"
+    };
+    JsonParser::Node root("");
+    ASSERT_EQ(JsonParser::Value(0, chs, &root), chs.size());
+    std::cout << root << std::endl;
 }
 
-TEST(JsonParserTest, LexicalTest4) {
-    std::vector<std::string> chs = {"{", "\"name\"", ":", "\"Tanaka\"", ",", "\"age\"", ":", "\"26\"", "}"};
-    JsonParser::Node* root = JsonParser::Parser(chs);
-    std::cout << *root << std::endl;
-    ASSERT_TRUE(true);
+TEST(JsonParserTest, ParserTest4) {
+    std::vector<std::string> chs = {
+        "\"ABC\""
+    };
+    JsonParser::Node root("");
+    ASSERT_EQ(JsonParser::Value(0, chs, &root), chs.size());
+    std::cout << root << std::endl;
 }
 
-TEST(JsonParserTest, LexicalTest5) {
-    std::vector<std::string> chs = {"{", "\"name\"", ":", "\"Tanaka\"", ",", "\"age\"", ":", "\"26\"", ",", "\"sex\"", ":", "\"male\"", "}"};
-    JsonParser::Node* root = JsonParser::Parser(chs);
-    std::cout << *root << std::endl;
-    ASSERT_TRUE(true);
+TEST(JsonParserTest, ParserTest5) {
+    std::vector<std::string> chs = {
+        "123"
+    };
+    JsonParser::Node root("");
+    ASSERT_EQ(JsonParser::Value(0, chs, &root), chs.size());
+    std::cout << root << std::endl;
 }
 
-TEST(JsonParserTest, LexicalTest6) {
-    std::vector<std::string> chs = {"{", "\"user_info\"", ":", "{", "\"user_id\"", ":", "\"A1234567\"", ",", "\"user_name\"", ":", "\"Yamada Taro\"", "}", "}"};
-    JsonParser::Node* root = JsonParser::Parser(chs);
-    std::cout << *root << std::endl;
-    ASSERT_TRUE(true);
+TEST(JsonParserTest, ParserTest6) {
+    std::vector<std::string> chs = {
+        "{",
+        "\"age\"",
+        ":",
+        "26",
+        ",",
+        "\"pi\"",
+        ":",
+        "3.14",
+        ",",
+        "\"planck_constant\"",
+        ":",
+        "6.62607e-34",
+        ",",
+        "\"negative_number\"",
+        ":",
+        "-123",
+        "}"
+    };
+    JsonParser::Node root("");
+    ASSERT_EQ(JsonParser::Value(0, chs, &root), chs.size());
+    std::cout << root << std::endl;
+}
+
+TEST(JsonParserTest, ParserTest7) {
+    std::vector<std::string> chs = {
+        "{",
+        "\"name\"",
+        ":",
+        "null",
+        ",",
+        "\"active_flag\"",
+        ":",
+        "true",
+        ",",
+        "\"delete_flag\"",
+        ":",
+        "false",
+        "}"
+    };
+    JsonParser::Node root("");
+    ASSERT_EQ(JsonParser::Value(0, chs, &root), chs.size());
+    std::cout << root << std::endl;
+}
+
+TEST(JsonParserTest, LexicalTest8) {
+    std::vector<std::string> chs = {
+        "{",
+        "\"user_info\"",
+        ":",
+        "{",
+        "\"user_id\"",
+        ":",
+        "\"A1234567\"",
+        ",",
+        "\"user_name\"",
+        ":",
+        "\"Yamada Taro\"",
+        "}",
+        "}"
+    };
+    JsonParser::Node root("");
+    ASSERT_EQ(JsonParser::Value(0, chs, &root), chs.size());
+    std::cout << root << std::endl;
 }
