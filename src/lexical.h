9b2da71412beb411e8037dce45462468e2a6b968
PANFACTORY <yutatanabe510@gmail.com>
2022-09-30 07:47:35 +0900

    ヘッダーオンリーに修正

--- src/lexical.h
+++ src/lexical.h
@@ -24,5 +24,81 @@ namespace JsonParser {
      * @param[in]   std::istream& ss            JSON format stream
      * @return      std::vector<std::string>    Token array
      */
-    std::vector<std::string> LexicalAnalyzer(std::istream& ss);
+    std::vector<std::string> LexicalAnalyzer(std::istream& ss) {
+        std::vector<std::string> chs;
+        char ch = ss.get();
+        while (!ss.eof()) {
+            switch (ch) {
+            case ' ':
+            case '\f':
+            case '\t':
+            case '\v':
+            case '\r':
+            case '\n':
+                ch = ss.get();
+                break;
+            case '"':
+                {
+                std::string str("\"");
+                ch = ss.get();
+                // TODO: "で閉じているかで判別する
+                while (!ss.eof() && ch != ':' && ch != '}' && ch != ',' && ch != ']') {
+                    str += ch;
+                    ch = ss.get();
+                }
+                str = str.substr(0, str.find_last_not_of(" \f\t\v\r\n") + 1);
+                // TODO: Check end of str is \".
+                chs.push_back(str);
+                }
+                break;
+            case '-':
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                {
+                std::string str(&ch);
+                ch = ss.get();
+                while (!ss.eof() && ch != '}' && ch != ',' && ch != ']') {
+                    str += ch;
+                    ch = ss.get();
+                }
+                str = str.substr(0, str.find_last_not_of(" \f\t\v\r\n") + 1);
+                if (!std::regex_match(str, std::regex("-?(0|[1-9]\\d*)(\\.\\d+)?(e[-+](0|[1-9]\\d*)(\\.\\d+)?)?"))) {
+                    throw std::runtime_error("At LexicalAnalyzer(): \"str\" is not a number.");
+                }
+                chs.push_back(str);
+                }
+                break;
+            case 't':
+            case 'f':
+            case 'n':
+                {
+                std::string str(&ch);
+                ch = ss.get();
+                while (!ss.eof() && ch != '}' && ch != ',' && ch != ']') {
+                    str += ch;
+                    ch = ss.get();
+                }
+                str = str.substr(0, str.find_last_not_of(" \f\t\v\r\n") + 1);
+                if (str != "true" && str != "false" && str != "null") {
+                    throw std::runtime_error("At LexicalAnalyzer(): \"str\" is neither true, false nor null.");
+                }
+                chs.push_back(str);
+                }
+                break;
+            default:
+                chs.push_back(std::string(&ch));
+                ch = ss.get();
+                break;
+            }
+        }
+        return chs;
+    }
 }
--- src/parser.h
+++ src/parser.h
@@ -13,17 +13,176 @@
 
 #include <vector>
 #include <string>
-#include <sstream>
-#include <cctype>
 
 #include "node.h"
 
 namespace JsonParser {
+    /**
+     * @brief   Definition of generative grammar
+     *
+     */
+    class Grammar {
+        friend Node* Parser(const std::vector<std::string> &chs);
+private:
+        /**
+         * @brief       Process <Array> in recursive descent parser.
+         *
+         * @param[in]   int index                           Current position in token array
+         * @param[in]   const std::vector<std::string> &chs Token array from lexical analyzer
+         * @param[in]   JsonParser::Node *parent            Pointer indicating the current AST node
+         * @return      int                                 Updated position in token array
+         */
+        static int Array(int index, const std::vector<std::string> &chs, Node *parent) {
+            if (index >= chs.size()) {
+                throw std::range_error("At Array(): \"index\" is over length of \"chs\".");
+            }
+            // std::cout << "Array() is called and index is " << index << std::endl;
+            switch (chs[index][0]) {
+            case ']':
+                parent->AppendArray();
+                return index + 1;
+            case '"':
+            case '-':
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case 'n':
+            case 't':
+            case 'f':
+            case '{':
+            case '[':
+                {
+                Node* child = new Node("");
+                int index_current = Grammar::Value(index, chs, child);
+                parent->AppendArray(child);
+
+                while (index_current < chs.size() && chs[index_current][0] == ',') {
+                    child = new Node("");
+                    index_current = Grammar::Value(index_current + 1, chs, child);
+                    parent->AppendArray(child);
+                }
+
+                return index_current + 1;
+                }
+            default:
+                throw std::range_error("At Array(): Unexpected error occurs.");
+                return 0;
+            }
+        }
+
+        /**
+         * @brief       Process <Object> in recursive descent parser.
+         *
+         * @param[in]   int index                           Current position in token array
+         * @param[in]   const std::vector<std::string> &chs Token array from lexical analyzer
+         * @param[in]   JsonParser::Node *parent            Pointer indicating the current AST node
+         * @return      int                                 Updated position in token array
+         */
+        static int Object(int index, const std::vector<std::string> &chs, Node *parent) {
+            if (index >= chs.size()) {
+                throw std::range_error("At Object(): \"index\" is over length of \"chs\".");
+            }
+            // std::cout << "Object() is called and index is " << index << std::endl;
+            switch (chs[index][0]) {
+            case '}':
+                parent->AppendChild();
+                return index + 1;
+            case '"':
+                {
+                Node* child = new Node(chs[index]);
+                int index_current = Grammar::Value(index + 2, chs, child);
+                parent->AppendChild(child);
+
+                while (index_current < chs.size() && chs[index_current][0] == ',') {
+                    // Check exists "key" of next object.
+                    if (index_current + 1 >= chs.size()) {
+                        throw std::range_error("At Object(): \"index\" is over length of \"chs\".");
+                    }
+                    if (chs[index_current + 1][0] != '"') {
+                        throw std::runtime_error("At Object(): '\"' is expected, however other character is set.");
+                    }
+                    child = new Node(chs[index_current + 1]);
+
+                    // Check exists ":" between "key" and Value.
+                    if (index_current + 2 >= chs.size()) {
+                        throw std::range_error("At Object(): \"index\" is over length of \"chs\".");
+                    }
+                    if (chs[index_current + 2][0] != ':') {
+                        throw std::runtime_error("At Object(): \":\" is expected, however other character is set.");
+                    }
+
+                    index_current = Grammar::Value(index_current + 3, chs, child);
+                    parent->AppendChild(child);
+                }
+
+                return index_current + 1;
+                }
+            default:
+                throw std::range_error("At Object(): Unexpected error occurs.");
+                return 0;
+            }
+        }
+
+        /**
+         * @brief       Process <Value> in recursive descent parser.
+         *
+         * @param[in]   int index                           Current position in token array
+         * @param[in]   const std::vector<std::string> &chs Token array from lexical analyzer
+         * @param[in]   JsonParser::Node *node              Pointer indicating the current AST node
+         * @return      int                                 Updated position in token array
+         */
+        static int Value(int index, const std::vector<std::string> &chs, Node *node) {
+            if (index >= chs.size()) {
+                throw std::range_error("At Value(): \"index\" is over length of \"chs\".");
+            }
+            // std::cout << "Value() is called and index is " << index << std::endl;
+            switch (chs[index][0]) {
+            case '"':
+            case '-':
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case 'n':
+            case 't':
+            case 'f':
+                node->SetValue(chs[index]);
+                return index + 1;
+            case '{':
+                return Grammar::Object(index + 1, chs, node);
+            case '[':
+                return Grammar::Array(index + 1, chs, node);
+            default:
+                throw std::range_error("At Value(): Unexpected error occurs.");
+                return 0;
+            }
+        }
+    };
+
     /**
      * @brief       Generate AST from token array with recursive descent parsing.
      *
      * @param[in]   const std::vector<std::string> &chs Token array from lexical analyzer
      * @return      JsonParser::Node*                   Pointer indicating the root AST node
      */
-    Node* Parser(const std::vector<std::string> &chs);
+    Node* Parser(const std::vector<std::string> &chs) {
+        Node* root = new Node("");
+        if (Grammar::Value(0, chs, root) != chs.size()) {
+            throw std::runtime_error("At Parser(): Return value of Value() is not equal to chs.size().");
+        }
+        return root;
+    }
 }
